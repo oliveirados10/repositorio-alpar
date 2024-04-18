c9eee5ab55ded26ba84e20be57f20a3f9b3d5b44
Emerson dos Santos <oliveiradossantos08@hotmail.com>
2024-04-18 15:18:51 -0300

    ajustado arq iluminacao do js

--- Ilumicacao/assets/javascript/iluminacao.js
+++ Ilumicacao/assets/javascript/iluminacao.js
@@ -1,4 +1,5 @@
 class ControleIluminacao {
+    static instance;
     constructor() {
         if (!ControleIluminacao.instance) {
             this.luzes = {
@@ -24,73 +25,22 @@ class ControleIluminacao {
 
 const controleIluminacao = new ControleIluminacao();
 
-const salaLigado = document.getElementById("salaLigado");
-const salaDesligado = document.getElementById("salaDesligado");
-
-const cozinhaLigado = document.getElementById("cozinhaLigado");
-const cozinhaDesligado = document.getElementById("cozinhaDesligado");
-
-const quartoLigado = document.getElementById("quartoLigado");
-const quartoDesligado = document.getElementById("quartoDesligado");
-
-const banheiroLigado = document.getElementById("banheiroLigado");
-const banheiroDesligado = document.getElementById("banheiroDesligado");
-
-const statusSala = document.getElementById('statusSala');
-const statusCozinha = document.getElementById('statusCozinha');
-const statusQuarto = document.getElementById('statusQuarto');
-const statusBanheiro = document.getElementById('statusBanheiro');
 
 controleIluminacao.adicionarObserver({
     atualizar: function (comodo, estado) {
         const statusElement = document.querySelector(`.status${comodo.charAt(0).toUpperCase() + comodo.slice(1)}`);
+        const comodoDiv = document.querySelector(`.${comodo}`);
         if (estado) {
             statusElement.innerHTML = '<br><strong>Luz Ligada</strong>';
-            statusElement.style.color = 'yellow';
+            statusElement.style.color = 'black';
+            statusElement.style.backgroundColor = 'yellow';
         } else {
-            statusElement.innerHTML = '<br><strong>Luz Desligada</strong>';
-            statusElement.style.color = 'red';
+            statusElement.innerHTML = '<br><strong>Luz Apagada</strong>';
+            statusElement.style.color = 'white';
+            statusElement.style.backgroundColor = 'red';
         }
     }
 });
 
-salaLigado.addEventListener('click', () => {
-    controleIluminacao.luzes.sala = true;
-    controleIluminacao.notificarObservers('sala');
-});
 
-salaDesligado.addEventListener('click', () => {
-    controleIluminacao.luzes.sala = false;
-    controleIluminacao.notificarObservers('sala');
-});
-
-cozinhaLigado.addEventListener('click', () => {
-    controleIluminacao.luzes.cozinha = true;
-    controleIluminacao.notificarObservers('cozinha');
-});
-
-cozinhaDesligado.addEventListener('click', () => {
-    controleIluminacao.luzes.cozinha = false;
-    controleIluminacao.notificarObservers('cozinha');
-});
-
-quartoLigado.addEventListener('click', () => {
-    controleIluminacao.luzes.quarto = true;
-    controleIluminacao.notificarObservers('quarto');
-});
-
-quartoDesligado.addEventListener('click', () => {
-    controleIluminacao.luzes.quarto = false;
-    controleIluminacao.notificarObservers('quarto');
-});
-
-banheiroLigado.addEventListener('click', () => {
-    controleIluminacao.luzes.banheiro = true;
-    controleIluminacao.notificarObservers('banheiro');
-});
-
-banheiroDesligado.addEventListener('click', () => {
-    controleIluminacao.luzes.banheiro = false;
-    controleIluminacao.notificarObservers('banheiro');
-});
 
